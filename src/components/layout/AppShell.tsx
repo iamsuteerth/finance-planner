45da68c3f7a9e0f1a44dce273e5e0dff0a85ba0d
iamsuteerth <suteerth@gmail.com>
2026-06-11 18:01:52 +0530

    Updated title and branding

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -118,7 +118,7 @@ export default function PlannerShell({
                   order={4}
                   fw={700}
                 >
-                  Finance Planner
+                  Worth Flow
                 </Title>
 
                 <Text
